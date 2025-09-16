b51d5e6c8c30d76911eca8df82b7f7914d76a685
Mohak Sharma <mohaksharma1412@gmail.com>
2025-09-17 00:21:10 +0530

    add vercel.json

--- Backend/server.js
+++ Backend/server.js
@@ -49,9 +49,11 @@ mongoose
   })
   .then(() => {
     console.log("MongoDB connected");
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    
   })
   .catch((err) => {
     console.error("MongoDB connection error:", err);
   });
+
+  export default app;
 
